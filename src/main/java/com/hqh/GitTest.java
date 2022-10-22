beff956142e427777b93d26556b2822aca8552be
houqingheng <hqh1689@163.com>
2022-10-22 21:23:34 +0800

    master first commit

--- src/main/java/com/hqh/GitTest.java
+++ src/main/java/com/hqh/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git test2");
         System.out.println("git test3");
         System.out.println("hotfix commit1");
+        System.out.println("master commit1");
     }
 }
